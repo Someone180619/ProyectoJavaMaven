aa06bac941ae826056e515e97d3aacce613b72b2
Someone180619 <51931813+Someone180619@users.noreply.github.com>
2021-07-28 13:28:37 -0500

    algo

--- src/main/java/seleccion.java
+++ src/main/java/seleccion.java
@@ -1,14 +1,16 @@
-public static void seleccion(int[]matrix){
-    int i, k, p, buffer, limit = matrix.lenght-1;
-    for(k = 0; k <= limit; k++){
-        p = k;
-        for(i = k+1; i <= limit; i++){
-            if(matrix[i] < matrix[p])
-            p = i;
-            if(p != k){
-                buffer = matrix[p];
-                matrix[p] = matrix[k];
-                matrix[k] = buffer;
+public class Seleccion {
+    public static void seleccion(int[]matrix){
+        int i, k, p, buffer, limit = matrix.lenght-1;
+        for(k = 0; k <= limit; k++){
+            p = k;
+            for(i = k+1; i <= limit; i++){
+                if(matrix[i] < matrix[p])
+                p = i;
+                if(p != k){
+                    buffer = matrix[p];
+                    matrix[p] = matrix[k];
+                    matrix[k] = buffer;
+                }
             }
         }
     }
